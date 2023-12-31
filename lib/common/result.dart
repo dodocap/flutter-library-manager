51f1e756e18f9324a9f6690ba5f854690bb6dd27
dodocap <dodocap@naver.com>
2023-12-31 20:35:18 +0900

    refactor: Result 변경

--- lib/common/result.dart
+++ lib/common/result.dart
@@ -1,13 +1,14 @@
 sealed class Result<T> {
-  const Result();
+  factory Result.success(T data) = Success<T>;
+  factory Result.error(String e) = Error<T>;
 }
 
-class Success<T> extends Result<T> {
+class Success<T> implements Result<T> {
   final T data;
   const Success(this.data);
 }
 
-class Error<T> extends Result<T> {
+class Error<T> implements Result<T> {
   final String error;
   const Error(this.error);
 }
